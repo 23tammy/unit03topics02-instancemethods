78f992e30308344fe512d29d2d3eaeb80bc263c4
tammy vu <tammy.vsk@gmail.com>
2023-03-03 12:37:23 -0600

    this other

--- Point2D.java
+++ Point2D.java
@@ -1,3 +1,39 @@
 public class Point2D {
-    
+    private double x;
+    private double y;
+
+    public Point2D (double x, double y){
+        this.x = x;
+        this.y = y;
+    } 
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distanceTo(Point2D other) {
+        this.x
+    }
+
+    public String toString (){
+        String result = "";
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
 }
