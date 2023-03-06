563ab2af7a37a0780ecdc1ed8e434e4bb9a17f9d
tammy vu <tammy.vsk@gmail.com>
2023-03-06 12:34:34 -0600

    intersects

--- Circle.java
+++ Circle.java
@@ -5,11 +5,28 @@ public class Circle {
     private Point2D center;
 
     public Circle (Point2D center, double radius){
-        radius = this.radius;
-        center = this.center;
+        this.radius = radius;
+        this.center = center;
     }
 
     public double getArea(){
-        
+        return ((Math.PI)*Math.pow(radius, 2));
+    }
+
+    public double getCircumference(){
+        return (2 * (Math.PI) * radius);
+    }
+
+    public boolean intersects(Circle other){
+
+    }
+
+    public static void main(String[] args){
+        Point2D p = new Point2D(1, 1);
+        Point2D p2 = new Point2D(3,3);
+        Circle c1 = new Circle(p, 3);
+        Circle c2 = new Circle(p2, 5);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getCircumference());
     }
 }
