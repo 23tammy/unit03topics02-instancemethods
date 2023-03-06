0fbb60dccbfd102f31441d6f8218f09a131ee9e4
tammy vu <tammy.vsk@gmail.com>
2023-03-06 11:52:22 -0600

    s

--- Circle.java
+++ Circle.java
@@ -1,3 +1,15 @@
+
 public class Circle {
-    
+
+    private double radius;
+    private Point2D center;
+
+    public Circle (Point2D center, double radius){
+        radius = this.radius;
+        center = this.center;
+    }
+
+    public double getArea(){
+        
+    }
 }
