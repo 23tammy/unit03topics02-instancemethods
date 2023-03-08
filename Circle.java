64f159ba0d29b91ba023daeaec44f2cee13d290b
tammy vu <tammy.vsk@gmail.com>
2023-03-08 11:40:28 -0600

    finfifnifnif

--- Circle.java
+++ Circle.java
@@ -19,9 +19,9 @@ public class Circle {
 
     public boolean intersects(Circle other){
         double distance = this.center.distanceTo(other.center);
-        System.out.println(distance + " <= " + this.radius +" + " + other.radius);
-    
+        double biggerCircle;
         double smallerCircle;
+    
         if (this.radius > other.radius){
             biggerCircle = this.radius;
             smallerCircle = other.radius;
@@ -29,22 +29,30 @@ public class Circle {
             biggerCircle = other.radius;
             smallerCircle = this.radius;
         }
-        if (distance <= this.radius && other.radius >= this.radius){
+        if (distance <= Math.abs(biggerCircle- smallerCircle)){
             return false;
         }else{
             return (distance <= this.radius + other.radius);
         } 
     }
 
+    public boolean contains(Circle other){
+        double distance = this.center.distanceTo(other.center);
+     
+        return(distance <= Math.abs(this.radius- other.radius));
+
+    }
+
 
 
     public static void main(String[] args){
-        Point2D p = new Point2D(1, 1);
-        Point2D p2 = new Point2D(0,0);
-        Circle c1 = new Circle(p, 3);
-        Circle c2 = new Circle(p2, 5);
-        System.out.println(c1.getArea());
-        System.out.println(c1.getCircumference());
+        Point2D p = new Point2D(6, 8);
+        Point2D p2 = new Point2D(1,1);
+        Circle c1 = new Circle(p, 5);
+        Circle c2 = new Circle(p2, 3);
+        // System.out.println(c1.getArea());
+        // System.out.println(c1.getCircumference());
         System.out.println(c1.intersects(c2));
+        System.out.println(c1.contains(c2));
     }
 }
