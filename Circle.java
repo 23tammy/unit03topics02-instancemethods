6897591b44fb87ae461b8473a5ea031bc145a56d
tammy vu <tammy.vsk@gmail.com>
2023-03-08 12:13:46 -0600

    fixed

--- Circle.java
+++ Circle.java
@@ -19,17 +19,8 @@ public class Circle {
 
     public boolean intersects(Circle other){
         double distance = this.center.distanceTo(other.center);
-        double biggerCircle;
-        double smallerCircle;
-    
-        if (this.radius > other.radius){
-            biggerCircle = this.radius;
-            smallerCircle = other.radius;
-        }else{
-            biggerCircle = other.radius;
-            smallerCircle = this.radius;
-        }
-        if (distance <= Math.abs(biggerCircle- smallerCircle)){
+     
+        if (distance <= Math.abs(this.radius- other.radius)){
             return false;
         }else{
             return (distance <= this.radius + other.radius);
@@ -39,7 +30,7 @@ public class Circle {
     public boolean contains(Circle other){
         double distance = this.center.distanceTo(other.center);
      
-        return(distance <= Math.abs(this.radius- other.radius));
+        return(distance <= this.radius- other.radius);
 
     }
 
@@ -48,8 +39,8 @@ public class Circle {
     public static void main(String[] args){
         Point2D p = new Point2D(1, 1);
         Point2D p2 = new Point2D(1,1);
-        Circle c1 = new Circle(p, 5);
-        Circle c2 = new Circle(p2, 3);
+        Circle c1 = new Circle(p, 3);
+        Circle c2 = new Circle(p2, 5);
         System.out.println(c1.getArea());
         System.out.println(c1.getCircumference());
         System.out.println(c1.intersects(c2));
